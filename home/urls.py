da28165844a4bbe0eecacc459ccebfaaedd64bf6
Nirmal Subramani <kumar.n.subramani@gmail.com>
2024-01-27 19:28:36 +0000

    add product detail page and test

--- home/urls.py
+++ home/urls.py
@@ -18,5 +18,6 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('', views.about, name='home'),
     
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -10,7 +10,18 @@ def all_products(request):
 
     context = {
         'products': products,
-    }
-    
+    }    
+
     return render(request, 'products/products.html', context)
 
+def product_detail(request, product_id):
+
+    """ A view to show individual product page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': products,
+    }    
+    
+    return render(request, 'products/product_detail.html', context)
