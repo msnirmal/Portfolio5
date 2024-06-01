bc79c56ae7467548344fcab9aa4d88e842205a9d
Nirmal Subramani <kumar.n.subramani@gmail.com>
2024-06-01 14:38:16 +0000

    add allowed host to fix django issue

--- craft_junction/settings.py
+++ craft_junction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6t3he=x*1-m0#38s^2bmi@2u+qh7fp8oyexj(7@t3@f7@jq^*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-msnirmal-portfolio5-4jrafj6u3ei.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-msnirmal-portfolio5-hmrku33ja2r.ws-eu114.gitpod.io']
 
 
 # Application definition
